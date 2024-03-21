4927e0533d8e7d411818751f34c7016a9e035d82
Hasanoruc466 <hasanoruc466@gmail.com>
2024-03-22 01:29:49 +0300

    commit-1

---
+++ src/main/java/com/hobs/hbblogbe/HbblogBeApplication.java
@@ -0,0 +1,13 @@
+package com.hobs.hbblogbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbblogBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HbblogBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hobs/hbblogbe/HbblogBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobs.hbblogbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbblogBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
